7c6fb68bc1155736ba18cc0f4391b082061cf6c8
Techori <harshrajsharma359@gmail.com>
2025-05-06 11:00:19 +0530

    Update ServiceHighlights.tsx

--- src/components/home/ServiceHighlights.tsx
+++ src/components/home/ServiceHighlights.tsx
@@ -84,6 +84,19 @@ const ServiceHighlights = () => {
             }
             link="/pages/WiFiSolutions.tsx"
           />
+                    <ServiceCard
+            title="Saas Product"
+            description="Reliable and secure wireless connectivity solutions for public spaces and business environments."
+            icon={
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-techori-purple" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M5 12.55a11 11 0 0 1 14.08 0"></path>
+                <path d="M1.42 9a16 16 0 0 1 21.16 0"></path>
+                <path d="M8.53 16.11a6 6 0 0 1 6.95 0"></path>
+                <line x1="12" y1="20" x2="12.01" y2="20"></line>
+              </svg>
+            }
+            link="/pages/WiFiSolutions.tsx"
+          />
           <ServiceCard
             title="Robotics Development"
             description="Cutting-edge robotics solutions to automate processes and enhance productivity in various industries."
