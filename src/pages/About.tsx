64ea36156b5e3a0cb6b95a064d47d4f21da83441
Techori <harshrajsharma359@gmail.com>
2025-05-15 19:54:30 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -404,7 +404,7 @@ const About = () => {
           </section>
 
           {/* Stats Section */}
-          <section className="py-16 bg-Black-50 text-White-900">
+          <section className="py-16 bg-Black-50 text-Black-900">
             <div className="container mx-auto px-4">
               <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
                 {[
